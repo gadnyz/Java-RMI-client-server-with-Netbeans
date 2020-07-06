5046a7a1c35c2ce51196548e9e068d1299c34d25
gad tambwe <gadnyumbaiza@gmail.com>
2020-07-07 01:12:47 +0200

    can client talk to server using rmi

--- Serveur/src/rmi/RMIServeur.java
+++ Serveur/src/rmi/RMIServeur.java
@@ -10,7 +10,6 @@ import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
 import java.util.Scanner;
 
 /**
